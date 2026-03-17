5cbe71062ae4d265419530a313b65d3f89850891
Evandro Santos Silva <evandro_07@outlook.com>
2026-03-16 22:46:32 -0400

    adicionado classe para criar tabela produtos

---
+++ projeto_curso/CriarTabelaProdutos.java
@@ -0,0 +1,32 @@
+package projeto_curso;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class CriarTabelaProdutos {
+    public static void main(String [] args){
+
+        try (Connection con = ConexaoBD.conectar();
+    Statement stmt = con.createStatement()) {
+
+        //query sql tabela produtos
+        String sql = "create table produtos(" +
+        "id_produto," +
+        "nome," +
+        "quantidade," +
+        "preco," +
+        "status)";
+
+        //executando a query
+        stmt.execute(sql);
+        System.out.println("tabela produtos criada com sucesso");
+
+            
+        } catch (SQLException e) {
+            System.err.println("Erro ao criar tabela produtos " + e.getMessage());
+            
+        }
+
+    }
+    
+}
